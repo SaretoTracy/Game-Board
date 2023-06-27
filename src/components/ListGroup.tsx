78673a32570cdc10ffaa9870dd9f058f0c83ba7c
Tracy Sareto <tracysareto@gmail.com>
2023-06-27 18:52:10 +0300

    practice conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,20 @@
 
 function ListGroup() {
-    const items =[
+    let items =[
         'Nairobi',
         'Nakuru',
         'Mombasa',
         'Eldoret'
     ];
+items =[];
+
   return (
     <>
         <h1>Lists</h1>
+        {items.length === 0 && <p>No item found!</p>}
     <ul className="list-group">
       {items.map((item)=>(
-    <li key={item }>{item}</li>
+    <li key={item  }>{item}</li>
       ))}
     </ul>
     </>
